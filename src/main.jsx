a71ccb87f124386c2374798f95c93a7b091715f0
Diego <dicisfg@gmail.com>
2026-05-12 16:42:34 -0500

    fix: configurar basename en BrowserRouter y base en vite para GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/API-Rick-and-Morty/">
       <App />
     </BrowserRouter>
   </StrictMode>,
